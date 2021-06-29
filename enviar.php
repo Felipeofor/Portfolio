fd8f4d7ae5eb04234c9af4a074e302b96ebbbbea
Ramos Felipe <felipeofor@gmail.com>
2021-06-29 15:45:07 -0300

    Vinculacion de formulario con php

--- enviar.php
+++ enviar.php
@@ -1,7 +1,8 @@
 <?php
 $nombre = $_POST['nombre'];
-$mail = $_POST['email'];
-$mensaje = $_POST['mensaje'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$subject = $_POST['subject'];
 ​
 $header = 'From: ' . $mail . " \r\n";
 $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
@@ -9,7 +10,7 @@ $header .= "Mime-Version: 1.0 \r\n";
 $header .= "Content-Type: text/plain";
 ​
 $mensaje = "Este mensaje fue enviado por " . $nombre . ",\r\n";
-$mensaje .= "Su e-mail es: " . $mail . " \r\n";
+$mensaje .= "Su e-mail es: " . $email . " \r\n";
 $mensaje .= "Mensaje: " . $_POST['mensaje'] . " \r\n";
 $mensaje .= "Enviado el " . date('d/m/Y', time());
 ​
