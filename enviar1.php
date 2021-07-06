7175da06a4d63d32fdfd2fd67c229883c2da33ae
Ramos Felipe <felipeofor@gmail.com>
2021-07-06 18:08:05 -0300

    error 404

---
+++ enviar1.php
@@ -0,0 +1,24 @@
+<?php
+$nombre = $_POST['nombre'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$subject = $_POST['subject'];
+
+$header = 'From: ' . $email . " \r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
+
+$message = "Este mensaje fue enviado por " . $nombre . ",\r\n";
+$message .= "Su e-mail es: " . $email . " \r\n";
+$message .= "Mensaje: " . $_POST['message'] . " \r\n";
+$message .= "Enviado el " . date('d/m/Y', time());
+
+$para = 'felipeofor@gmail.com'; // El mail a donde van a llegar los mensajes
+$asunto = 'Mensaje de mi sitio web';
+
+mail($para, $asunto, utf8_decode($message), $header);
+
+header('Location:index.html');
+
+?>
